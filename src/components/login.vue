793c38ea5634b5432741969d73ea8716e24819a6
fengfox <fengfox044@163.com>
2018-06-28 07:28:47 +0800

    add login

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,40 +1,83 @@
-<template>  
-<div class="login">
-  <div class="page-title">登录</div>
-  <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
-  <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
-  <div class="submit">
-  <mt-button size="large" type="primary">确认</mt-button>
-  </div>
-  </div>
+<template>
+    <div>
+        <div class="login-wrap" v-show="showLogin">
+            <h3>登录</h3>
+            <p v-show="showTishi">{{tishi}}</p>
+            <input type="text" placeholder="请输入用户名" v-model="username">
+            <input type="password" placeholder="请输入密码" v-model="password">
+            <button v-on:click="login">登录</button>
+           <!-- <span v-on:click="ToRegister">没有账号？马上注册</span>-->
+        </div>
 
-</template>  
+        <div class="register-wrap" v-show="showRegister">
+            <h3>注册</h3>
+            <p v-show="showTishi">{{tishi}}</p>
+            <input type="text" placeholder="请输入用户名" v-model="newUsername">
+            <input type="password" placeholder="请输入密码" v-model="newPassword">
+            <button v-on:click="register">注册</button>
+            <span v-on:click="ToLogin">已有账号？马上登录</span>
+        </div>
+    </div>
+</template>
+
+<style>
+    .login-wrap{text-align:center;}
+    input{display:block; width:250px; height:40px; line-height:40px; margin:0 auto; margin-bottom: 10px; outline:none; border:1px solid #888; padding:10px; box-sizing:border-box;}
+    p{color:red;}
+    button{display:block; width:250px; height:40px; line-height: 40px; margin:0 auto; border:none; background-color:#41b883; color:#fff; font-size:16px; margin-bottom:5px;}
+    span{cursor:pointer;}
+    span:hover{color:#41b883;}
+</style>
 
 <script>
-export default {  
-  
-} 
- 
+  import {setCookie,getCookie} from '../assets/js/cookie.js'
+    export default{
+       mounted(){
+  /*页面挂载获取cookie，如果存在username的cookie，则跳转到主页，不需登录*/
+    if(getCookie('username')){
+        this.$router.push('/home')
+    }
+  },
+        data(){
+            return{
+                showLogin: true,
+                showRegister: false,
+                showTishi: false,
+                tishi: '',
+                username: '',
+                password: '',
+                newUsername: '',
+                newPassword: ''
+            }
+        },
+         methods:{
+
+          login(){
+        if(this.username == "" || this.password == ""){
+            alert("请输入用户名或密码")
+        }else{
+            let data = {'userName':this.username,'pwd':this.password}
+            /*接口请求*/
+            this.$http.post('/login',data).then((res)=>{
+                console.log(res)
+             /*接口的传值是(-1,该用户不存在),(0,密码错误)，同时还会检测管理员账号的值*/
+              if(res.data.data == false){
+                  this.tishi = "用户账号密码不匹配"
+                  this.showTishi = true
+              }
+             
+              else{
+                  this.tishi = "登录成功"
+                  this.showTishi = true
+                  setCookie('username',this.username,1000*60)
+                  setTimeout(function(){
+                      this.$router.push('/home')
+                  }.bind(this),1000)
+              }
+          })
+      }
+         }
+
+    }
+  }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.login
-{
-  padding: 10%;
-}
-.submit
-{
-  margin-top: 20px;
-}
-.page-title
-{
-  font-size:32px;
-  text-align: center;
-  margin-bottom: 5%;
-  
-}
-
-
-
-</style>
